039f81fb7f9d3554de4911b7a147915dd1965986
Ezequiel Lucero <ezequiel.noct@gmail.com>
2021-10-22 01:03:30 -0300

    Prueba git cpanel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -166,7 +166,7 @@
                 </ul>
 
                 <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                  <span>Saved reports</span>
+                  <span>Editando de prueba</span>
                   <a class="d-flex align-items-center text-muted" href="#">
                     <span data-feather="plus-circle"></span>
                   </a>
