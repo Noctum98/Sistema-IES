2747f224e9818200ff7ce7cf4ff171c60577d4e7
Ezequiel Lucero <ezequiel.noct@gmail.com>
2021-11-09 13:13:14 -0300

    Excel tribual corregido

--- resources/views/excel/tribual_mesa.blade.php
+++ resources/views/excel/tribual_mesa.blade.php
@@ -3,13 +3,17 @@
 		<tr>
 		    <th scope="col">Espacio Curricular</th>
 			<th scope="col">Año</th>
-		    <th scope="col">Fecha primer llamado</th>
-		    <th scope="col">Hora primer llamado</th>
-		    <th scope="col">Fecha segundo llamado</th>
-		    <th scope="col">Hora segundo llamado</th>
-		    <th scope="col">Presidente</th>
-		    <th scope="col">Primer Vocal</th>
-		    <th scope="col">Segundo Vocal</th>
+		    <th scope="col">Fecha Primer llamado</th>
+		    <th scope="col">Hora Primer llamado</th>
+			<th scope="col">Presidente Primer llamado</th>
+		    <th scope="col">Primer Vocal Primer llamado</th>
+		    <th scope="col">Segundo Vocal Primer llamado</th>
+		    <th scope="col">Fecha Segundo llamado</th>
+		    <th scope="col">Hora Segundo llamado</th>
+			<th scope="col">Presidente Segundo llamado</th>
+		    <th scope="col">Primer Vocal Segundo llamado</th>
+		    <th scope="col">Segundo Vocal Segundo llamado</th>
+
 		</tr>
 	</thead>
 	<tbody>
@@ -19,11 +23,14 @@
 		<td>{{ $mesa['materia']['año'] }}</td>
         <td>{{ date_format(new DateTime( $mesa['fecha'] ), 'd-m-Y') }}</td>
         <td>{{date_format(new DateTime( $mesa['fecha'] ), 'H:i') }}</td>
-        <td>{{  date_format(new DateTime( $mesa['fecha_segundo'] ), 'd-m-Y') }}</td>
-        <td>{{  date_format(new DateTime( $mesa['fecha_segundo'] ), 'H:i') }}</td>
-        <td>{{ $mesa['presidente'] }}</td>
+		<td>{{ $mesa['presidente'] }}</td>
         <td>{{ $mesa['primer_vocal'] }}</td>
         <td>{{ $mesa['segundo_vocal'] }}</td>
+        <td>{{  date_format(new DateTime( $mesa['fecha_segundo'] ), 'd-m-Y') }}</td>
+        <td>{{  date_format(new DateTime( $mesa['fecha_segundo'] ), 'H:i') }}</td>
+		<td>{{ $mesa['presidente_segundo'] }}</td>
+        <td>{{ $mesa['primer_vocal_segundo'] }}</td>
+        <td>{{ $mesa['segundo_vocal_segundo'] }}</td>
 		</tr>
         @endforeach
 	</tbody>
