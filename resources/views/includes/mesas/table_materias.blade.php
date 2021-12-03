ee755085128a63c62cfbc57b39f24cae4d81a7ae
Ezequiel Lucero <ezequiel.noct@gmail.com>
2021-12-03 09:36:59 -0300

    cambios a testear

--- resources/views/includes/mesas/table_materias.blade.php
+++ resources/views/includes/mesas/table_materias.blade.php
@@ -1,4 +1,4 @@
-<!--
+
 <tr>
     <td>
         {{ $materia->nombre }}
@@ -81,54 +81,4 @@
         <a href="{{route('mesa.inscriptos',['id'=>$materia->id])}}" class="btn-sm btn-success">Descargar excel</a>
         @endif
     </td>
-</tr>
--->
-<tr>
-    <td>
-        {{ $materia->nombre }}
-    </td>
-    @if($instancia->tipo == 0)
-    <td>
-        @if($materia->mesas)
-        @if($materia->mesa($instancia->id,$materia->id) && $materia->mesa($instancia->id,$materia->id)->fecha)
-        <strong>
-            {{
-                date_format(new DateTime($materia->mesa($instancia->id,$materia->id)->fecha),
-                'd-m-Y H:i:s'
-            )}}
-        </strong>
-        @endif
-        @endif
-    </td>
-    <td>
-        @if($materia->mesas)
-        @if($materia->mesa($instancia->id,$materia->id) && $materia->mesa($instancia->id,$materia->id)->fecha_segundo)
-        <strong>
-            {{
-                date_format(new DateTime($materia->mesa($instancia->id,$materia->id)->fecha_segundo),
-                'd-m-Y H:i:s'
-            )}}
-        </strong>
-        @endif
-        @endif
-    </td>
-    <td>
-        @if($instancia->tipo == 0)
-        @if($materia->mesas)
-        @if($materia->mesa($instancia->id,$materia->id))
-        <a href="{{route('mesa.inscriptos',['id'=>$mesa->id])}}" class="btn-sm btn-secondary">Ver inscriptos</a>
-            @if(Auth::user()->rol == 'rol_admin')
-            <a href="{{route('mesa.inscriptos',['id'=>$mesa->id])}}" data-toggle="modal" data-target="#editModal{{$materia->id}}" class="btn-sm btn-primary">Editar mesa</a>
-            @endif      
-        @include('includes.mesas.edit_mesa')
-        @else
-        <a href="{{route('mesa.inscriptos',['id'=>$materia->id])}}" class="btn-sm btn-warning" data-toggle="modal" data-target="#exampleModal{{$materia->id}}">Configurar mesa</a>
-        @endif
-        @endif
-        @include('includes.mesas.config_mesa')
-        @else
-        <a href="{{route('mesa.inscriptos',['id'=>$mesa->id])}}" class="btn-sm btn-secondary">Ver inscriptos</a>
-        @endif
-    </td>
-    @endif
 </tr>
