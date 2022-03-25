6161845c6c54ac62103474c3d9b7997e1c3ab87d
Ezequiel Lucero <ezequiel.noct@gmail.com>
2022-03-25 10:01:46 -0300

    Buscador corregido 2

--- resources/views/preinscripcion/admin.blade.php
+++ resources/views/preinscripcion/admin.blade.php
@@ -12,14 +12,12 @@
 		</a>
 		<a href="{{route('pre.articulo')}}" class="ml-2 btn btn-secondary">Articulo 7mo</a>
 	</div>
-	<div id="buscador-alumnos">
-		<form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0" method="GET" action="#" id="buscador">
-			<div class="input-group mt-3" >
-				<input class="form-control ml-3" type="text" id="busqueda" placeholder="Buscar preinscripcion" aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-				<button type="submit" class="btn btn-primary"><i class="fas fa-search"></i></button>
-			</div>
-		</form>
-	</div>
+	<form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0" method="GET" action="#" id="buscador">
+		<div class="input-group mt-3">
+			<input class="form-control ml-3" type="text" id="busqueda" placeholder="Buscar preinscripcion" aria-label="Search for..." aria-describedby="btnNavbarSearch" />
+			<button type="submit" class="btn btn-primary"><i class="fas fa-search"></i></button>
+		</div>
+	</form>
 
 
 	@if(!empty($carreras))
