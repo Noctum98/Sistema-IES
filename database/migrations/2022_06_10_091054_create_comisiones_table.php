63b72eaeb8dd542429c0c2a03500a7ae617d7b03
Ezequiel Lucero <ezequiel.noct@gmail.com>
2022-06-10 09:17:53 -0300

    Estructura: comisiones creada

---
+++ database/migrations/2022_06_10_091054_create_comisiones_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComisionesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comisiones', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('carrera_id')->constrained('carreras');
+            $table->integer('año');
+            $table->string('nombre');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comisiones');
+    }
+}
