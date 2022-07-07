b8a35ebe0adc866f7a9e7aa71781f4c13c8767b9
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2022-07-06 23:48:45 -0300

    Cambios en cargos, vistas porcentaje y el recuperatorio acepta A y de 0 - 100

--- app/Http/Controllers/ProcesoCalificacionController.php
+++ app/Http/Controllers/ProcesoCalificacionController.php
@@ -20,40 +20,36 @@ class ProcesoCalificacionController extends Controller
 
         $ausente = false;
 
-        if(is_numeric($request['porcentaje']) || $request['porcentaje'] === 0){
-            $rules = ['required','numeric','max:100'];
-        }else{
-            $rules = ['required','string','regex:/^[A?a?]/'];
+        if (is_numeric($request['porcentaje']) || $request['porcentaje'] === 0) {
+            $rules = ['required', 'numeric', 'max:100'];
+        } else {
+            $rules = ['required', 'string', 'regex:/^[A?a?]/'];
             $ausente = true;
         }
 
-        $validate = Validator::make($request->all(),[
-            'porcentaje' => $rules
+        $validate = Validator::make($request->all(), [
+            'porcentaje' => $rules,
         ]);
 
-        if(!$validate->fails())
-        {
+        if (!$validate->fails()) {
             $procesoCalificacion = ProcesoCalificacion::where([
                 'proceso_id' => $request['proceso_id'],
-                'calificacion_id' => $request['calificacion_id']
+                'calificacion_id' => $request['calificacion_id'],
             ])->first();
-    
-            if($procesoCalificacion)
-            {
-                if(!$ausente)
-                {
+
+            if ($procesoCalificacion) {
+                if (!$ausente) {
                     $procesoCalificacion->porcentaje = $request['porcentaje'];
-                    $procesoCalificacion->nota = $this->calcularNota((int) $request['porcentaje']);
-                }else{
+                    $procesoCalificacion->nota = $this->calcularNota((int)$request['porcentaje']);
+                } else {
                     $procesoCalificacion->porcentaje = -1;
                     $procesoCalificacion->nota = -1;
                 }
                 $procesoCalificacion->update();
-            }else{
-                $request['nota'] = $ausente ? $request['porcentaje'] : $this->calcularNota((int) $request['porcentaje']);
+            } else {
+                $request['nota'] = $ausente ? $request['porcentaje'] : $this->calcularNota((int)$request['porcentaje']);
 
-                if($ausente)
-                {
+                if ($ausente) {
                     $request['porcentaje'] = -1;
                     $request['nota'] = -1;
                 }
@@ -62,93 +58,104 @@ class ProcesoCalificacionController extends Controller
             }
 
             $reponse = $procesoCalificacion;
-        }else{
+        } else {
             $reponse = [
                 'code' => 200,
-                'errors' => $validate->errors()
+                'errors' => $validate->errors(),
             ];
         }
-       
 
-        return response()->json($reponse,200);
+
+        return response()->json($reponse, 200);
     }
 
     public function crearRecuperatorio(Request $request)
     {
+
         $ausente = false;
-        if($request['porcentaje'] && is_numeric($request['porcentaje'])){
-            $rules = ['required','numeric','max:100'];
-        }else{
-            $rules = ['required','string','regex:/^[A?a?]/'];
+        if ($request['porcentaje'] && is_numeric($request['porcentaje'])) {
+            $rules = ['required', 'numeric', 'max:100'];
+        } elseif ($request['porcentaje'] === '0') {
+            $rules = ['required', 'numeric', 'max:100'];
+        } else {
+            $rules = ['required', 'string', 'regex:/^[A?a?]/'];
             $ausente = true;
         }
-        $validate = Validator::make($request->all(),[
-            'porcentaje' => $rules
+        $validate = Validator::make($request->all(), [
+            'porcentaje' => $rules,
         ]);
 
-        if(!$validate->fails())
-        {
+        if (!$validate->fails()) {
             $procesoCalificacion = ProcesoCalificacion::where([
                 'proceso_id' => $request['proceso_id'],
-                'calificacion_id' => $request['calificacion_id']
+                'calificacion_id' => $request['calificacion_id'],
             ])->first();
 
             $procesoCalificacion->porcentaje_recuperatorio = $request['porcentaje'];
 
             if (!$ausente) {
-                $procesoCalificacion->nota_recuperatorio = $this->calcularNota((int) $request['porcentaje']);
-            }else{
+                $procesoCalificacion->nota_recuperatorio = $this->calcularNota((int)$request['porcentaje']);
+            } else {
                 $procesoCalificacion->nota_recuperatorio = 'A';
             }
 
             $procesoCalificacion->update();
 
             $response = $procesoCalificacion;
-        }else{
+        } else {
             $response = [
                 'code' => 200,
-                'errors' => $validate->errors()
+                'errors' => $validate->errors(),
             ];
         }
 
-        return response()->json($response,200);
+        return response()->json($response, 200);
     }
 
     private function calcularNota($porcentaje)
     {
-        if($porcentaje == 0)
-        {
+        if ($porcentaje == 0) {
             $nota = 0;
-        }else if($porcentaje >= 1 && $porcentaje <= 19 )
-        {
-            $nota = 1;
-        }else if($porcentaje >=20 && $porcentaje <=39)
-        {
-            $nota = 2;
-        }else if($porcentaje >=40 && $porcentaje <=59)
-        {
-            $nota = 3;
-        }else if($porcentaje >=60 && $porcentaje <=65)
-        {
-            $nota = 4;
-        }else if($porcentaje >=66 && $porcentaje <=71)
-        {
-            $nota = 5;
-        }else if($porcentaje >=72 && $porcentaje <=77)
-        {
-            $nota = 6;
-        }else if($porcentaje >=78 && $porcentaje <=83)
-        {
-            $nota = 7;
-        }else if($porcentaje >=84 && $porcentaje <=89)
-        {
-            $nota = 8;
-        }else if($porcentaje >=90 && $porcentaje <=95)
-        {
-            $nota = 9;
-        }else if($porcentaje >=96 && $porcentaje <=100)
-        {
-            $nota = 10;
+        } else {
+            if ($porcentaje >= 1 && $porcentaje <= 19) {
+                $nota = 1;
+            } else {
+                if ($porcentaje >= 20 && $porcentaje <= 39) {
+                    $nota = 2;
+                } else {
+                    if ($porcentaje >= 40 && $porcentaje <= 59) {
+                        $nota = 3;
+                    } else {
+                        if ($porcentaje >= 60 && $porcentaje <= 65) {
+                            $nota = 4;
+                        } else {
+                            if ($porcentaje >= 66 && $porcentaje <= 71) {
+                                $nota = 5;
+                            } else {
+                                if ($porcentaje >= 72 && $porcentaje <= 77) {
+                                    $nota = 6;
+                                } else {
+                                    if ($porcentaje >= 78 && $porcentaje <= 83) {
+                                        $nota = 7;
+                                    } else {
+                                        if ($porcentaje >= 84 && $porcentaje <= 89) {
+                                            $nota = 8;
+                                        } else {
+                                            if ($porcentaje >= 90 && $porcentaje <= 95) {
+                                                $nota = 9;
+                                            } else {
+                                                if ($porcentaje >= 96 && $porcentaje <= 100) {
+                                                    $nota = 10;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         return $nota;
