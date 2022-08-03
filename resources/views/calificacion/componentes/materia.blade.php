9c31fdf9657a802c0cf820676ddc8f7434a8ed30
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2022-08-03 18:20:33 -0300

    Muestra todos los módulos del profesor si no hay discriminación por módulo

---
+++ resources/views/calificacion/componentes/materia.blade.php
@@ -0,0 +1,18 @@
+@foreach($cargos as $cargo)
+    <div class="border border-secondary border-top-0 pl-2 pt-1">
+        @foreach($cargo->materias as $materia)
+
+            <h5>{{$cargo->nombre.' - '. $cargo->carrera->nombre.'('.$cargo->carrera->sede->nombre.')'}}</h5>
+            @if(isset($materia))
+                <a type="button" href="{{ route($ruta,['materia_id'=>$materia->id,'cargo_id'=>$cargo->id]) }}"
+                   class="list-group-item list-group-item-action border-top mt-2 text-primary"
+                   title="Ver calificaciones"
+                >
+                    <strong>
+                        Módulo: {{ $materia->nombre.' ( '.ucwords($materia->carrera->turno).' | Res: '.$materia->carrera->resolucion.' )' }}
+                    </strong>
+                </a>
+            @endif
+        @endforeach
+    </div>
+@endforeach
