53fed70c0d6877c4087e6ce69771f00df3aab285
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2022-09-17 23:21:05 -0300

    :ambulance: Listo el pdf

--- resources/views/pdfs/mesa_generar_pdf.blade.php
+++ resources/views/pdfs/mesa_generar_pdf.blade.php
@@ -1,80 +1,158 @@
 @extends('layouts.pdf')
+<style>
+    table, tr, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+        font-size: 10pt;
+    }
+    th, td {
+        padding-top: 10px;
+        padding-bottom: 10px;
+        padding-left: 5px;
+    }
+</style>
 
 <div class="container alumno">
     <img src="{{ 'images/logo-dge-iesvu.png' }}" style="width: 100%"  alt="DATA-IESVU">
     <h5><u> {{$carrera->nombre}} Res: {{$carrera->resolucion}}</u></h5>
     <h5><u>Turno: {{$instancia->nombre}}</u></h5>
-    <h5><u>Mesas ordenadas por año</u> {{$carrera->sede()->first()->nombre}}</h5>
+    <h5><u>Mesas ordenadas por año</u> <span class="text-right">{{$carrera->sede()->first()->nombre}}</span></h5>
 
     <div class="col-md-12">
+        <div class="table">
+                <table class="table" border="0" >
+                    <thead class="thead-dark">
+                    <tr>
+                        <th  rowspan="2" scope="col">Espacio Curricular</th>
+                        <th colspan="2" scope="col">Primer Llamado</th>
+                        <th colspan="3" scope="col">Integrante</th>
+                    </tr>
+                    <tr>
+                        <th scope="col">Fecha</th>
+                        <th scope="col">Hora</th>
+                        <th scope="col">Presidente</th>
+                        <th scope="col">Primer Vocal</th>
+                        <th scope="col">Segundo Vocal </th>
+                    </tr>
 
-        <div class="datos-alumno">
-            <ul class="col-md-6">
-                <h3>Datos de Personales</h3>
-                <li><strong>Nombre:</strong> {{ $instancia->nombre }}</li>
-                {{--					<li><strong>Apellidos:</strong> {{ $alumno->apellidos }}</li>--}}
-                {{--					<li><strong>Edad:</strong> {{ $alumno->edad }} años</li>--}}
-                {{--					<li><strong>Telefono:</strong> {{ $alumno->telefono }}</li>--}}
-                {{--					<li><strong>Email:</strong> {{ $alumno->email }}</li>--}}
-                {{--					<li><strong>Fecha de nacimiento: </strong> {{ $alumno->fecha }}</li>--}}
-                {{--					<li><strong>N° de documento: </strong> {{ $alumno->dni }}</li>--}}
-                {{--					<li><strong>CUIL: </strong> {{ $alumno->cuil }}</li>--}}
-                {{--					<li><strong>Género: </strong> {{ ucwords($alumno->genero) }}</li>--}}
-            </ul>
-        </div>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td colspan="6" align="center"> <b>Espacios curriculares 1 <sup>er</sup> Año </b></td>
+                    </tr>
+                    @foreach($carrera->materias()->get() as $materia )
+                        @if($materia->año == 1)
+                        <tr>
+                            <td>{{$materia->nombre}}</td>
+                            <td @empty($materia->mesa($instancia->id))
+                                    colspan="5"
+                                    @endempty
+                            >
+                                @if($materia->mesa($instancia->id))
+                                {{ date_format(new DateTime($materia->mesa($instancia->id)->fecha), 'd-m-Y') }}
+                                @else
+                                    No tiene mesa asignada
+                                @endif
+                            </td>
+                            @if($materia->mesa($instancia->id))
 
+                            <td>{{ date_format(new DateTime($materia->mesa($instancia->id)->fecha), 'H:i') }}</td>
+                            <td>{{$materia->mesa($instancia->id)->presidente}}</td>
+                            <td>{{$materia->mesa($instancia->id)->primer_vocal}}</td>
+                            <td>{{$materia->mesa($instancia->id)->segundo_vocal}}</td>
 
-        {{--		<ul class="col-md-6">--}}
-        {{--			<li>--}}
-        {{--				<h2>Datos de Inscripción</h3>--}}
-        {{--			</li>--}}
-        {{--			<li><strong>Situación:</strong>{{ str_replace('_',' ',ucwords($alumno->regularidad)) }}</li>--}}
-        {{--			<li>--}}
-        {{--				<strong>Inscripto a:</strong>--}}
-        {{--				<br>--}}
-        {{--				@foreach($alumno->carreras as $carrera)--}}
-        {{--				_ {{ $carrera->nombre.' ('.ucwords($carrera->turno).') - '.$carrera->sede->nombre }}--}}
-        {{--				<br>--}}
-        {{--				Año: {{ $alumno->procesoCarrera($carrera->id,$alumno->id)->año }}--}}
-        {{--				@endforeach--}}
-        {{--			</li>--}}
-        {{--			<li> <strong>Materias:</strong>--}}
-        {{--				<br>--}}
-        {{--				@foreach($alumno->procesos as $proceso)--}}
-        {{--				<p>{{$proceso->materia->nombre}}</p>--}}
-        {{--				@endforeach--}}
-        {{--			</li>--}}
+                            @endif
+{{--                            <td>{{ $mesa['materia']['año'] }}</td>--}}
+{{--                            <td>{{ date_format(new DateTime( $mesa['fecha'] ), 'd-m-Y') }}</td>--}}
+{{--                            <td>{{date_format(new DateTime( $mesa['fecha'] ), 'H:i') }}</td>--}}
+{{--                            <td>{{ $mesa['presidente'] }}</td>--}}
+{{--                            <td>{{ $mesa['primer_vocal'] }}</td>--}}
+{{--                            <td>{{ $mesa['segundo_vocal'] }}</td>--}}
+{{--                            <td>{{ $mesa['fecha_segundo'] ? date_format(new DateTime( $mesa['fecha_segundo'] ), 'd-m-Y') : '' }}</td>--}}
+{{--                            <td>{{ $mesa['fecha_segundo'] ? date_format(new DateTime( $mesa['fecha_segundo'] ), 'H:i') : ''}}</td>--}}
+{{--                            <td>{{ $mesa['presidente_segundo'] }}</td>--}}
+{{--                            <td>{{ $mesa['primer_vocal_segundo'] }}</td>--}}
+{{--                            <td>{{ $mesa['segundo_vocal_segundo'] }}</td>--}}
+                        </tr>
+                        @endif
+                    @endforeach
+                    <tr>
+                        <td colspan="6" align="center"> <b>Espacios curriculares 2 <sup>do</sup> Año </b></td>
+                    </tr>
+                    @foreach($carrera->materias()->get() as $materia )
+                        @if($materia->año == 2)
+                            <tr>
+                                <td>{{$materia->nombre}}</td>
+                                <td align="center" @empty($materia->mesa($instancia->id))
+                                        colspan="5"
+                                        @endempty
+                                >
+                                    @if($materia->mesa($instancia->id))
+                                        {{ date_format(new DateTime($materia->mesa($instancia->id)->fecha), 'd-m-Y') }}
+                                    @else
+                                        No tiene mesa asignada
+                                    @endif
+                                </td>
+                                @if($materia->mesa($instancia->id))
 
-        {{--			@if($alumno->año == 1)--}}
-        {{--			<li><strong>Escuela Secundaria: </strong> {{ $alumno->escuela_s }}</li>--}}
-        {{--			<li><strong>Articulo Séptimo: </strong> {{ $alumno->articulo_septimo ? 'Si' : 'No' }} </li>--}}
-        {{--			<li><strong>Finalizo Escuela Secundaria: </strong> {{ $alumno->escolaridad ? 'Si' : 'No' }}</li>--}}
-        {{--			<li><strong>Materias que adeuda de secundario: </strong>--}}
-        {{--				@if(!$alumno->materias_s)--}}
-        {{--				Ninguna--}}
-        {{--				@else--}}
-        {{--				{{ str_replace(';',' - ',$alumno->materias_s) }}--}}
-        {{--				@endif--}}
-        {{--			</li>--}}
-        {{--			<li><strong>Presento título secundario:</strong> {{$alumno->titulo_s ? 'Si' : 'No'}} </li>--}}
-        {{--			@endif--}}
-        {{--		</ul>--}}
+                                    <td align="center">{{ date_format(new DateTime($materia->mesa($instancia->id)->fecha), 'H:i') }}</td>
+                                    <td>{{$materia->mesa($instancia->id)->presidente}}</td>
+                                    <td>{{$materia->mesa($instancia->id)->primer_vocal}}</td>
+                                    <td>{{$materia->mesa($instancia->id)->segundo_vocal}}</td>
 
+                                @endif
+                                {{--                            <td>{{ $mesa['materia']['año'] }}</td>--}}
+                                {{--                            <td>{{ date_format(new DateTime( $mesa['fecha'] ), 'd-m-Y') }}</td>--}}
+                                {{--                            <td>{{date_format(new DateTime( $mesa['fecha'] ), 'H:i') }}</td>--}}
+                                {{--                            <td>{{ $mesa['presidente'] }}</td>--}}
+                                {{--                            <td>{{ $mesa['primer_vocal'] }}</td>--}}
+                                {{--                            <td>{{ $mesa['segundo_vocal'] }}</td>--}}
+                                {{--                            <td>{{ $mesa['fecha_segundo'] ? date_format(new DateTime( $mesa['fecha_segundo'] ), 'd-m-Y') : '' }}</td>--}}
+                                {{--                            <td>{{ $mesa['fecha_segundo'] ? date_format(new DateTime( $mesa['fecha_segundo'] ), 'H:i') : ''}}</td>--}}
+                                {{--                            <td>{{ $mesa['presidente_segundo'] }}</td>--}}
+                                {{--                            <td>{{ $mesa['primer_vocal_segundo'] }}</td>--}}
+                                {{--                            <td>{{ $mesa['segundo_vocal_segundo'] }}</td>--}}
+                            </tr>
+                        @endif
+                    @endforeach
+                    <tr>
+                        <td colspan="6" align="center"> <b>Espacios curriculares 3 <sup>er</sup> Año </b></td>
+                    </tr>
+                    @foreach($carrera->materias()->get() as $materia )
+                        @if($materia->año == 3)
+                            <tr>
+                                <td>{{$materia->nombre}}</td>
+                                <td @empty($materia->mesa($instancia->id))
+                                        colspan="5"
+                                        @endempty
+                                >
+                                    @if($materia->mesa($instancia->id))
+                                        {{ date_format(new DateTime($materia->mesa($instancia->id)->fecha), 'd-m-Y') }}
+                                    @else
+                                        No tiene mesa asignada
+                                    @endif
+                                </td>
+                                @if($materia->mesa($instancia->id))
 
-        {{--		<ul class="col-md-6">--}}
-        {{--			<li>--}}
-        {{--				<h2>Datos de Domicilio</h3>--}}
-        {{--			</li>--}}
-        {{--			<li><strong>Nacionalidad:</strong> {{ ucwords($alumno->nacionalidad) }}</li>--}}
-        {{--			<li><strong>Provincia:</strong> {{ ucwords($alumno->provincia) }}</li>--}}
-        {{--			<li><strong>Localidad:</strong> {{ ucwords($alumno->localidad) }}</li>--}}
-        {{--			<li><strong>Calle: </strong> {{ $alumno->calle }}</li>--}}
-        {{--			<li><strong>N° de calle: </strong> {{ $alumno->n_calle }}</li>--}}
-        {{--			<li><strong>Barrio: </strong> {{ $alumno->barrio }}</li>--}}
-        {{--			<li><strong>Manzana: </strong> {{ $alumno->manzana }} </li>--}}
-        {{--			<li><strong>Casa: </strong> {{ $alumno->casa }}</li>--}}
-        {{--			<li><strong>Código Postal: </strong> {{ $alumno->codigo_postal }}</li>--}}
-        {{--		</ul>--}}
+                                    <td>{{ date_format(new DateTime($materia->mesa($instancia->id)->fecha), 'H:i') }}</td>
+                                    <td>{{$materia->mesa($instancia->id)->presidente}}</td>
+                                    <td>{{$materia->mesa($instancia->id)->primer_vocal}}</td>
+                                    <td>{{$materia->mesa($instancia->id)->segundo_vocal}}</td>
 
-    </div>
-</div>
+                                @endif
+                                {{--                            <td>{{ $mesa['materia']['año'] }}</td>--}}
+                                {{--                            <td>{{ date_format(new DateTime( $mesa['fecha'] ), 'd-m-Y') }}</td>--}}
+                                {{--                            <td>{{date_format(new DateTime( $mesa['fecha'] ), 'H:i') }}</td>--}}
+                                {{--                            <td>{{ $mesa['presidente'] }}</td>--}}
+                                {{--                            <td>{{ $mesa['primer_vocal'] }}</td>--}}
+                                {{--                            <td>{{ $mesa['segundo_vocal'] }}</td>--}}
+                                {{--                            <td>{{ $mesa['fecha_segundo'] ? date_format(new DateTime( $mesa['fecha_segundo'] ), 'd-m-Y') : '' }}</td>--}}
+                                {{--                            <td>{{ $mesa['fecha_segundo'] ? date_format(new DateTime( $mesa['fecha_segundo'] ), 'H:i') : ''}}</td>--}}
+                                {{--                            <td>{{ $mesa['presidente_segundo'] }}</td>--}}
+                                {{--                            <td>{{ $mesa['primer_vocal_segundo'] }}</td>--}}
+                                {{--                            <td>{{ $mesa['segundo_vocal_segundo'] }}</td>--}}
+                            </tr>
+                        @endif
+                    @endforeach
+                    </tbody>
+                </table>
