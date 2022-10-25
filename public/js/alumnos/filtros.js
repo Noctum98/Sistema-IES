8145516e3992d2dda5d238fa18c9a0f32684673d
Ezequiel Lucero <ezequiel.noct@gmail.com>
2022-10-25 11:50:37 -0300

    Ajuste filtros

--- public/js/alumnos/filtros.js
+++ public/js/alumnos/filtros.js
@@ -1,23 +1,5 @@
 $(document).ready(function () {
     let data = $("#buscador-alumnos").serializeArray();
-    let filtros = [];
-
-    $("#aplicar_filtros").click(function () {
-
-        data.forEach(element => {
-            if (element.value != '') {
-                filtros.push(element);
-            }
-        });
-
-        if (filtros.length != 0) {
-            $("#filtros_button").html("<span class='text-info' id='filters_count'>(" + filtros.length + ")</span>")
-        } else {
-            $("#filtros_button").html("");
-        }
-    });
-
-   
     let url = '/excel/descargarFiltro/?';
 
     data.forEach((element, key) => {
