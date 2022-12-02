429a83768383d4647ca25f48ae334739cdf9c0fc
Noctum98 <62156836+Noctum98@users.noreply.github.com>
2022-12-02 08:59:59 -0300

    Update PreinscripcionController.php

--- app/Http/Controllers/PreinscripcionController.php
+++ app/Http/Controllers/PreinscripcionController.php
@@ -63,7 +63,7 @@ class PreinscripcionController extends Controller
         
         $carrera = Carrera::find($id);
         $error = '';
-        $carreras_abiertas = [18,9,1,15,6,2,27,25,37,40,41];
+        $carreras_abiertas = [18,9,1,15,6,2,27,25,37,41];
 
         if(!in_array($carrera->id,$carreras_abiertas) && !Session::has('preinscripciones'))
         {
