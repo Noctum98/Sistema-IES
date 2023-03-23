c4f7e915c40d387aafcc5d571ba380ad6d0f5f6e
Noctum98 <62156836+Noctum98@users.noreply.github.com>
2023-03-23 17:02:16 -0300

    Update MatriculacionController.php

--- app/Http/Controllers/MatriculacionController.php
+++ app/Http/Controllers/MatriculacionController.php
@@ -33,9 +33,9 @@ class MatriculacionController extends Controller
     ) {
         $this->procesoService = $procesoService;
         $this->mailService = $mailService;
-        $this->middleware('app.auth');
+        $this->middleware('app.auth',['only'=>['index']);
 
-        $this->middleware('app.roles:admin',['only'=>['create']]);
+        //$this->middleware('app.roles:admin',['only'=>['create']]);
 
         //$this->middleware('app.auth',['only'=>['create','edit','index']]);
         // $this->middleware('app.roles:admin-coordinador-seccionAlumnos-regente',['only'=>['create','edit','index']]);
