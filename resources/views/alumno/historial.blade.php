71f46374b8d9ee7c46ce93f148fc1418512d24a9
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2023-04-18 23:13:40 -0300

    :twisted_rightwards_arrows: Agregando recuadros al histórico. Corrigiendo bordes.

--- resources/views/alumno/historial.blade.php
+++ resources/views/alumno/historial.blade.php
@@ -39,7 +39,7 @@
                         @endif
                     </li>
                 </ul>
-                <span class="text-success" id="password_rees"></span>
+
                 <div class="col-12">
                     <h4>Materias</h4>
                     <div class="row mt-1">
@@ -47,9 +47,9 @@
                             <div class="col-md-12 mt-2 pt-1 ">
                                 <h5 class="text-center">{{ $nivel }}° Año</h5>
                                 <div class="row mt-1 border-1">
-                                    <div class="col-3 h-100 border ">Materia</div>
-                                    <div class="col-9 ">
-                                        <div class="row">
+                                    <div class="col-3 border-1 ">Materia</div>
+                                    <div class="col-9 border-1">
+                                        <div class="row ">
                                             <div class="col-2 border-top-0 text-center">Estado</div>
                                             <div class="col-2 border-1 border-left text-center border-bottom-0">Cierre</div>
                                             <div class="col-2 border-1 border-left text-center border-bottom-0">Nota Final</div>
@@ -96,48 +96,12 @@
             </div>
 
             <div class="d-inline col-md-12">
-                @if(Auth::user()->hasRole('regente') || Auth::user()->hasRole('coordinador') || Auth::user()->hasRole('seccionAlumnos') || Auth::user()->hasRole('admin'))
-
-                    @if(!$alumno->user_id || !$alumno->aprobado)
-                        <a href="{{ route('crear_usuario_alumno',['id'=>$alumno->id]) }}"
-                           class="col-md-2 mt-4 btn btn-sm btn-success">
-                            Confirmar alumno
-                        </a>
-                    @endif
-                    @if(isset($carrera) && $carrera)
-                        <a href="{{ route('matriculacion.edit',['alumno_id'=>$alumno->id,'carrera_id'=>$carrera->id]) }}"
-                           class="col-md-2 ml-2 mr-2 mt-4 btn btn-sm btn-warning">
-                            Corregir datos
-                        </a>
-                    @endif
-                @endif
-
                 <a href="{{ route('descargar_ficha',$alumno->id) }}" class="col-md-2 mt-4 btn btn-sm btn-primary"><i
                         class="fas fa-download"></i> Descargar PDF</a>
-
-                @if((Session::has('coordinador') || Session::has('admin') || Session::has('regente')) && $alumno->user)
-                    <button
-                        class="ml-2 mt-4 btn btn-sm btn-danger {{ !$alumno->user->activo ? 'd-none' : '' }} desactivar"
-                        id="desactivar-{{$alumno->user->id}}">
-                        Desactivar
-                    </button>
-
-                    <button
-                        class="ml-2 mt-4 btn btn-sm btn-success {{ $alumno->user->activo == 1 ? 'd-none' : '' }} activar"
-                        id="activar-{{$alumno->user->id}}">
-                        Activar
-                    </button>
-
-                    <button class="ml-2 mt-4 btn btn-sm btn-info btn-password" id="{{$alumno->user->id}}">
-                        Restablecer Contraseña
-                    </button>
-                @endif
             </div>
         </div>
     </div>
 @endsection
 @section('scripts')
     <script src="{{ asset('js/user/carreras.js') }}"></script>
-    <script src="{{ asset('js/user/activar.js') }}"></script>
-
 @endsection
