3b590dff6c6ec5806d3a5d96abb9d91c5d8f057a
Ezequiel Lucero <ezequiel.noct@gmail.com>
2023-04-25 11:35:31 -0300

    Ajustes boton eliminar alumno

--- resources/views/alumno/modals/correo_eliminar.blade.php
+++ resources/views/alumno/modals/correo_eliminar.blade.php
@@ -29,9 +29,9 @@
                         <textarea name="motivo" id="motivo" cols="20" rows="5" class="form-control"></textarea>
                     </div>
                     <div class="alert alert-danger">
-                        !ATENCIÓN! Al pulsar "Eliminar matriculación" se borrará tanto el alumno, como los procesos a los que se ha inscripto en esa carrera.
+                        !ATENCIÓN! Al pulsar "Eliminar inscripción" se borrará tanto el alumno, como los procesos a los que se ha inscripto en esa carrera.
                     </div>
-                    <button type="submit" class="btn btn-sm btn-danger">Eliminar matriculación</button>
+                    <button type="submit" class="btn btn-sm btn-danger">Eliminar inscripción</button>
                     <button type="button" class="btn btn-sm btn-warning" data-bs-dismiss="modal">Cerrar</button>
 
                 </form>
