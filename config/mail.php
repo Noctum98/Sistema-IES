eeeab17604c879ecb4a28b0f7f840c1e6765c648
Ezequiel Lucero <ezequiel.noct@gmail.com>
2023-08-28 15:43:54 -0300

    Ajuste email

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => 'smtp.gmail.com',
-            'port' => 587,
+            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => 'data@iesvu.edu.ar',
-            'password' => 'dataiesvu*2022/',
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'auth_mode' => null,
         ],
