414eafe10afbb594e481c5fc746580c8f2802c7d
Ezequiel Lucero <ezequiel.noct@gmail.com>
2023-08-28 13:07:31 -0300

    Ajuste correo

--- config/mail.php
+++ config/mail.php
@@ -40,7 +40,7 @@ return [
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
-            'password' => "work&2023",
+            'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'auth_mode' => null,
         ],
