aa6c167550dbe546035cb821b5e8bc4b54f83d2f
Ezequiel Lucero <ezequiel.noct@gmail.com>
2023-09-15 12:14:45 -0300

    Ajustes planilla excel modular

--- app/Exports/Sheets/PlanillaModularCargoSheet.php
+++ app/Exports/Sheets/PlanillaModularCargoSheet.php
@@ -27,7 +27,8 @@ class PlanillaModularCargoSheet implements FromView, WithTitle, WithEvents
         return view('excel.planilla_notas_modular', [
             'materia' => $this->data_array['materia'],
             'procesos' => $this->data_array['procesos'],
-            'ciclo_lectivo' => $this->data_array['ciclo_lectivo']
+            'ciclo_lectivo' => $this->data_array['ciclo_lectivo'],
+            'cargos' => $this->data_array['materia']->cargos
         ]);
     }
 
@@ -41,9 +42,12 @@ class PlanillaModularCargoSheet implements FromView, WithTitle, WithEvents
         // Get Worksheet
         $active_sheet = $event->sheet->getDelegate();
         $active_sheet->getColumnDimension('A')->setAutoSize(true);
+
         foreach (range('B', 'G') as $columnID) {
+
             $active_sheet->getColumnDimension($columnID)
                 ->setWidth('25');
+            $active_sheet->getStyle($columnID)->getAlignment()->setWrapText(true);
         }
 
         $active_sheet->getRowDimension('1')->setRowHeight(40);
@@ -53,10 +57,14 @@ class PlanillaModularCargoSheet implements FromView, WithTitle, WithEvents
         $active_sheet->getStyle('A1:B1')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('fbbc04');
         $active_sheet->getStyle('A2:B2')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('fbbc04');
         $active_sheet->getStyle('A3:B3')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('fbbc04');
+        $active_sheet->getStyle('C1')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('fbbc04');
+
         $active_sheet->getStyle('A4:F4')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('46bdc6');
         $active_sheet->getStyle('A4:G4')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('46bdc6');
 
-        $active_sheet->getStyle('A1:B1')->getBorders()
+
+
+        $active_sheet->getStyle('A1:C1')->getBorders()
             ->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
         $active_sheet->getStyle('A2:B2')->getBorders()
             ->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
@@ -66,7 +74,5 @@ class PlanillaModularCargoSheet implements FromView, WithTitle, WithEvents
             ->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
         $active_sheet->getStyle('A4:G4')->getBorders()
             ->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
-        $active_sheet->getStyle('A4:F4')->getAlignment()->setWrapText(true);
-        $active_sheet->getStyle('A2:B2')->getAlignment()->setWrapText(true);
     }
 }
--- resources/views/excel/planilla_notas_modular.blade.php
+++ resources/views/excel/planilla_notas_modular.blade.php
@@ -1,16 +1,26 @@
 <table class="table table-striped f30">
     <thead>
         <tr>
-            <th><b>UNIDAD ACADEMICA:</b></th>
-            <th><b>{{$materia->carrera->sede->nombre}} </b></th>
+            <th><b>UNIDAD ACADEMICA: {{$materia->carrera->sede->nombre}}</b></th>
+            <th><b>CARRERA: {{ $materia->carrera->nombre.' ( '.ucwords($materia->carrera->turno).' )' }} </b></th>
+            <th><b>Docentes:
+                @foreach($cargos as $cargo)
+                    @foreach($cargo->users as $key => $user)
+                        {{ mb_strtoupper($user->apellido).' '.ucwords($user->nombre) }}
+                        @if (!$loop->last)
+                        -
+                        @endif
+                    @endforeach
+                @endforeach
+                </b></th>
         </tr>
         <tr>
-            <th><b>CARRERA:</b></th>
-            <th> <b> {{ $materia->carrera->nombre.' ( '.ucwords($materia->carrera->turno).' )' }}</b></th>
+            <th><b>MÓDULO: {{ $materia->nombre }} </b></th>
+            <th> <b>RESOLUCION: {{ $materia->carrera->resolucion }} </b></th>
         </tr>
         <tr>
-            <th><b>RESOLUCION: </b></th>
-            <th style="text-align: left;"><b> {{ $materia->carrera->resolucion }} </b></th>
+            <th><b>AÑO: {{ $materia->año }}
+            <th style="text-align: left;"><b>Ciclo Lectivo: {{ $ciclo_lectivo }} </b></th>
         </tr>
         <tr>
             <th scope="col">Apeliido y Nombre</th>
@@ -26,22 +36,22 @@
         @foreach($procesos as $proceso)
         @if($proceso->procesoModularOne)
         <tr>
-            <td>
+            <td style="text-align: right;">
                 {{ optional($proceso->alumno)->apellidos_nombres }}
             </td>
-            <td class="text-center">
+            <td style="text-align: right;">
                 {{ $proceso->procesoModularOne->promedio_final_nota }}
             </td>
-            <td class="text-center">
+            <td style="text-align: right;">
                 {{ $proceso->procesoModularOne->asistencia_final_porcentaje }} %
             </td>
-            <td class="text-center">
+            <td style="text-align: right;">
                 {{ $proceso->procesoModularOne->trabajo_final_nota ? $proceso->procesoModularOne->trabajo_final_nota : '-'}}
             </td>
-            <td class="text-center">
+            <td style="text-align: right;">
                 {{ $proceso->procesoModularOne->nota_final_nota ? $proceso->procesoModularOne->nota_final_nota : '-'}}
             </td>
-            <td class="text-center">
+            <td style="text-align: right;">
                 {{ $proceso->procesoModularOne->nota_global ? $proceso->procesoModularOne->nota_global : '-' }}
             </td>
             <td>
