0725d6011472507df5e3a5fe3e173c8751891d57
Ezequiel Lucero <ezequiel.noct@gmail.com>
2023-11-24 11:35:40 -0300

    Ajuste planilla

--- resources/views/alumno/encuesta_socioeconomica/form2.blade.php
+++ resources/views/alumno/encuesta_socioeconomica/form2.blade.php
@@ -18,34 +18,94 @@
                     <h4>Motivos que te animan a seguir estudiando:</h4>
                 </label>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="motivo_para_estudiar" id="distancia_ies1" value="superación personal" {{ old('distancia_ies') === 'superación personal' ? 'checked' : '' }}>
-                    <label class="form-check-label" for="distancia_ies1">
-                    Superación personal
+                    <input class="form-check-input" type="radio" name="motivo_para_estudiar" id="motivo_para_estudiar1" value="superación personal" {{ old('distancia_ies') === 'superación personal' ? 'checked' : '' }}>
+                    <label class="form-check-label" for="motivo_para_estudiar1">
+                        Superación personal
                     </label>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="motivo_para_estudiar" id="distancia_ies2" value="mejorar las condiciones del trabajo que poseo" {{ old('distancia_ies') === 'mejorar las condiciones del trabajo que poseo' ? 'checked' : '' }}>
-                    <label class="form-check-label" for="distancia_ies2">
-                    Mejorar las condiciones del trabajo que poseo
+                    <input class="form-check-input" type="radio" name="motivo_para_estudiar" id="motivo_para_estudiar2" value="mejorar las condiciones del trabajo que poseo" {{ old('distancia_ies') === 'mejorar las condiciones del trabajo que poseo' ? 'checked' : '' }}>
+                    <label class="form-check-label" for="motivo_para_estudiar2">
+                        Mejorar las condiciones del trabajo que poseo
                     </label>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="motivo_para_estudiar" id="distancia_ies2" value="mejorar las condiciones del trabajo que poseo" {{ old('distancia_ies') === 'mejorar las condiciones del trabajo que poseo' ? 'checked' : '' }}>
-                    <label class="form-check-label" for="distancia_ies2">
-                    Mejorar las condiciones del trabajo que poseo
+                    <input class="form-check-input" type="radio" name="motivo_para_estudiar" id="motivo_para_estudiar3" value="tener mayor acceso laboral" {{ old('distancia_ies') === 'tener mayor acceso laboral' ? 'checked' : '' }}>
+                    <label class="form-check-label" for="motivo_para_estudiar3">
+                        Tener mayor acceso laboral
                     </label>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="motivo_para_estudiar" id="distancia_ies2" value="mejorar las condiciones del trabajo que poseo" {{ old('distancia_ies') === 'mejorar las condiciones del trabajo que poseo' ? 'checked' : '' }}>
-                    <label class="form-check-label" for="distancia_ies2">
-                    Mejorar las condiciones del trabajo que poseo
+                    <input class="form-check-input" type="radio" name="motivo_para_estudiar" id="motivo_para_estudiar6" value="cercanía con mi domicilio" {{ old('distancia_ies') === 'cercanía con mi domicilio' ? 'checked' : '' }}>
+                    <label class="form-check-label" for="motivo_para_estudiar6">
+                        Cercanía con mi domicilio
                     </label>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="motivo_para_estudiar" id="distancia_ies2" value="mejorar las condiciones del trabajo que poseo" {{ old('distancia_ies') === 'mejorar las condiciones del trabajo que poseo' ? 'checked' : '' }}>
-                    <label class="form-check-label" for="distancia_ies2">
-                    Mejorar las condiciones del trabajo que poseo
+                    <input class="form-check-input" type="radio" name="motivo_para_estudiar" id="motivo_para_estudiar4" value="porque me gusta la carrera" {{ old('distancia_ies') === 'porque me gusta la carrera' ? 'checked' : '' }}>
+                    <label class="form-check-label" for="motivo_para_estudiar4">
+                        Mejorar las condiciones del trabajo que poseo
                     </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="motivo_para_estudiar" id="motivo_para_estudiar5" value="otro" {{ old('distancia_ies') === 'otro' ? 'checked' : '' }}>
+                    <label class="form-check-label" for="motivo_para_estudiar5">
+                        Otros
+                    </label>
+                </div>
+                <div class="form-group d-none" id="otra_identidad_genero">
+                    <label for="motivo_para_estudiar_otro">Cual?</label>
+                    <input type="text" class="form-control mt-2" id="motivo_para_estudiar_otro" name="motivo_para_estudiar_otro" value="{{ old('motivo_para_estudiar_otro') }}">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card mb-3">
+        <div class="card-body">
+            <div class="form-group">
+                <label for="nombre_preferido" class="text-primary">
+                    <h4>¿Cómo conociste el instituto?</h4>
+                </label>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="motivo_para_estudiar" id="motivo_para_estudiar1" value="por otra persona" {{ old('distancia_ies') === 'por otra persona' ? 'checked' : '' }}>
+                    <label class="form-check-label" for="motivo_para_estudiar1">
+                    Por otra persona
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="motivo_para_estudiar" id="motivo_para_estudiar2" value="mejorar las condiciones del trabajo que poseo" {{ old('distancia_ies') === 'mejorar las condiciones del trabajo que poseo' ? 'checked' : '' }}>
+                    <label class="form-check-label" for="motivo_para_estudiar2">
+                        Mejorar las condiciones del trabajo que poseo
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="motivo_para_estudiar" id="motivo_para_estudiar3" value="tener mayor acceso laboral" {{ old('distancia_ies') === 'tener mayor acceso laboral' ? 'checked' : '' }}>
+                    <label class="form-check-label" for="motivo_para_estudiar3">
+                        Tener mayor acceso laboral
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="motivo_para_estudiar" id="motivo_para_estudiar6" value="cercanía con mi domicilio" {{ old('distancia_ies') === 'cercanía con mi domicilio' ? 'checked' : '' }}>
+                    <label class="form-check-label" for="motivo_para_estudiar6">
+                        Cercanía con mi domicilio
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="motivo_para_estudiar" id="motivo_para_estudiar4" value="porque me gusta la carrera" {{ old('distancia_ies') === 'porque me gusta la carrera' ? 'checked' : '' }}>
+                    <label class="form-check-label" for="motivo_para_estudiar4">
+                        Mejorar las condiciones del trabajo que poseo
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="motivo_para_estudiar" id="motivo_para_estudiar5" value="otro" {{ old('distancia_ies') === 'otro' ? 'checked' : '' }}>
+                    <label class="form-check-label" for="motivo_para_estudiar5">
+                        Otros
+                    </label>
+                </div>
+                <div class="form-group d-none" id="otra_identidad_genero">
+                    <label for="motivo_para_estudiar_otro">Cual?</label>
+                    <input type="text" class="form-control mt-2" id="motivo_para_estudiar_otro" name="motivo_para_estudiar_otro" value="{{ old('motivo_para_estudiar_otro') }}">
                 </div>
             </div>
         </div>
