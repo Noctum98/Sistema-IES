3eb8e37c4b01082fd6226861881d3c045e494f4e
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2023-11-27 22:39:46 -0300

    :test_tube: Mostrando session

--- resources/views/procesoModular/listado.blade.php
+++ resources/views/procesoModular/listado.blade.php
@@ -25,12 +25,12 @@
 
     </style>
     <div class="container-fluid w-100" id="container-scroll">
-        @if(Session::has('message'))
+        @if(@session('message'))
             <div class="alert alert-success alert-dismissible" role="alert">
                 <button type="button" class="close"
                         data-dismiss="alert" aria-label="Close">
                     <span aria-hidden="true">&times;</span></button>
-                {{Session::get('message')}}
+                {{ @session('message') }}
             </div>
         @endif
         <div class="row m-0">
