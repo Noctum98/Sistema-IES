5723de86cc15798eca0c3ddcf03042e330677703
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2023-12-15 12:20:43 -0300

    Agregando función ajax

--- public/js/proceso_modular/cargaComprobacionNotas.js
+++ public/js/proceso_modular/cargaComprobacionNotas.js
@@ -1,19 +1,18 @@
-
-    $(document).ready(function(){
-    $('.check-notes-btn').click(function(e) {
+$(document).ready(function () {
+    $('.check-notes-btn').click(function (e) {
         e.preventDefault();
 
         let url = $(this).data('url'); // Obtén la URL desde el data attribute
         let trOneProceso = $(this).data('trOne'); // Obtén la URL desde el data attribute
-
+console.log(trOneProceso)
         $.ajax({
             url: url,
             type: "GET",
-            success: function(response) {
+            success: function (response) {
                 // Aquí actualizas el <tr> con la respuesta del servidor
                 // Recuerda que necesitas tener un identificador único para seleccionar el correcto <tr>
                 // Por ejemplo: $('#unique-tr-id').html(response);
-                $('#'+trOneProceso).html(response);
+                $('#' + trOneProceso).html(response);
             }
         });
 
