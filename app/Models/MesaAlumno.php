cbb1d80ca3674618db4209efb708c9d97e46c9e6
germdz <gerardo.montivero@gmail.com>
2024-03-12 21:19:07 -0300

    No mostrar datos si la mesa está dada de baja.

--- app/Models/MesaAlumno.php
+++ app/Models/MesaAlumno.php
@@ -2,12 +2,68 @@
 
 namespace App\Models;
 
+use Eloquent;
 use Exception;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\MesaAlumno
+ *
+ * @property int $id
+ * @property int|null $mesa_id
+ * @property int|null $alumno_id
+ * @property int|null $materia_id
+ * @property int|null $segundo_llamado
+ * @property int|null $instancia_id
+ * @property string $nombres
+ * @property string $apellidos
+ * @property string $dni
+ * @property string $correo
+ * @property string $telefono
+ * @property int $estado_baja
+ * @property int|null $confirmado
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property int|null $user_id
+ * @property string|null $motivo_baja
+ * @property Carbon|null $deleted_at
+ * @property-read ActaVolante|null $acta_volante
+ * @property-read Alumno|null $alumno
+ * @property-read Instancia|null $instancia
+ * @property-read Materia|null $materia
+ * @property-read Mesa|null $mesa
+ * @property-read User|null $user
+ * @method static Builder|MesaAlumno newModelQuery()
+ * @method static Builder|MesaAlumno newQuery()
+ * @method static Builder|MesaAlumno onlyTrashed()
+ * @method static Builder|MesaAlumno query()
+ * @method static Builder|MesaAlumno whereAlumnoId($value)
+ * @method static Builder|MesaAlumno whereApellidos($value)
+ * @method static Builder|MesaAlumno whereConfirmado($value)
+ * @method static Builder|MesaAlumno whereCorreo($value)
+ * @method static Builder|MesaAlumno whereCreatedAt($value)
+ * @method static Builder|MesaAlumno whereDeletedAt($value)
+ * @method static Builder|MesaAlumno whereDni($value)
+ * @method static Builder|MesaAlumno whereEstadoBaja($value)
+ * @method static Builder|MesaAlumno whereId($value)
+ * @method static Builder|MesaAlumno whereInstanciaId($value)
+ * @method static Builder|MesaAlumno whereMateriaId($value)
+ * @method static Builder|MesaAlumno whereMesaId($value)
+ * @method static Builder|MesaAlumno whereMotivoBaja($value)
+ * @method static Builder|MesaAlumno whereNombres($value)
+ * @method static Builder|MesaAlumno whereSegundoLlamado($value)
+ * @method static Builder|MesaAlumno whereTelefono($value)
+ * @method static Builder|MesaAlumno whereUpdatedAt($value)
+ * @method static Builder|MesaAlumno whereUserId($value)
+ * @method static Builder|MesaAlumno withTrashed()
+ * @method static Builder|MesaAlumno withoutTrashed()
+ * @mixin Eloquent
+ */
 class MesaAlumno extends Model
 {
     use HasFactory;
