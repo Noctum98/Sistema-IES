c36afc6e6c7dec6ead9203452bdb020a9baef1a0
Ezequiel Lucero <ezequiel.noct@gmail.com>
2024-04-17 17:31:23 -0300

    Ajustes

--- app/Services/AlumnoCarreraService.php
+++ app/Services/AlumnoCarreraService.php
@@ -13,15 +13,19 @@ class AlumnoCarreraService
         ->first();
 
         $datos = [
-            'alumno_id' => $inscripcion->alumno_id,
-            'carrera_id' => $inscripcion->carrera_id,
+            'alumno_id' => $request['alumno_id'],
+            'carrera_id' => $request['carrera_id'],
             'año' => $request['año'],
-            'fecha_primera_acreditacion' => $inscripcion->fecha_primera_acreditacion,
-            'fecha_ultima_acreditacion' => $inscripcion->fecha_ultima_acreditacion,
-            'cohorte' => $inscripcion->cohorte,
-            'legajo_completo' => $inscripcion->legajo_completo,
             'ciclo_lectivo' => $request['ciclo_lectivo']
         ];
+
+        if($inscripcion)
+        {
+            $datos['fecha_primera_acreditacion'] = $inscripcion->fecha_primera_acreditacion;
+            $datos['fecha_ultima_acreditacion'] = $inscripcion->fecha_ultima_acreditacion;
+            $datos['cohorte'] = $inscripcion->cohorte;
+            $datos['legajo_completo'] = $inscripcion->legajo_completo;
+        }
 
         return $datos;
     }
