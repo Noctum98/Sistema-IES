9aecae438ae4d5893dc363599f32df949325a7b8
germdz <gerardo.montivero@gmail.com>
2024-04-26 09:17:09 -0300

    Cambiando modular2 por modular 70/30

--- resources/views/componentes/tipoCarrera/tradicionalToDisciplinar.blade.php
+++ resources/views/componentes/tipoCarrera/tradicionalToDisciplinar.blade.php
@@ -3,7 +3,10 @@
         Disciplinar
         @break
     @case('tradicional2')
-        Disciplinar2
+        Disciplinar 70/30
+        @break
+    @case('modular2')
+        Modular 70/30
         @break
     @default
         {{ ucfirst($tipo) }}
