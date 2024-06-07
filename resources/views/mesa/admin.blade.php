afde8a83e494001aa284ee7c8e48e823836a5f7f
Noctum98 <62156836+Noctum98@users.noreply.github.com>
2024-06-07 15:10:06 -0300

    Update admin.blade.php

--- resources/views/mesa/admin.blade.php
+++ resources/views/mesa/admin.blade.php
@@ -58,9 +58,6 @@
                             Editar Instancia
                         </button>
                         <a href="{{ route('mesas.resumen',$instancia->id) }}" class="btn btn-sm btn-light">Ver Informe</a>
-                        @if(Session::has('admin'))
-                            <a href="#" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#delete{{ $instancia->id }}">Eliminar Instancia</a>
-                        @endif
                         @endif
                         @if(Session::has('admin') || Session::has('coordinador') || Session::has('regente'))
                         <a class="btn btn-sm btn-success" data-bs-toggle="modal" data-bs-target="#vistaSeleccionMateria" id="vistaMateria" data-loader="{{$instancia->id}}-materia" data-attr="{{ route('materia.vista_materia', ['instancia' => $instancia->id]) }}">
