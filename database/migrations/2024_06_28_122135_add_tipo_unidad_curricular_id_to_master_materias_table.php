87d1ffbdfc572f02da885afb2d9503a783fef0b4
germdz <gerardo.montivero@gmail.com>
2024-07-01 17:50:47 -0300

    Cambio compatibilidad con tipo_materias

--- database/migrations/2024_06_28_122135_add_tipo_unidad_curricular_id_to_master_materias_table.php
+++ database/migrations/2024_06_28_122135_add_tipo_unidad_curricular_id_to_master_materias_table.php
@@ -14,7 +14,7 @@ class AddTipoUnidadCurricularIdToMasterMateriasTable extends Migration
     public function up()
     {
         Schema::table('master_materias', function (Blueprint $table) {
-            $table->foreignUuid('tipo_unidad_curricular_id')->nullable();
+            $table->unsignedBigInteger('tipo_unidad_curricular_id')->nullable();
             $table->foreign('tipo_unidad_curricular_id')
                 ->references('id')->
                 on('tipo_materias');
