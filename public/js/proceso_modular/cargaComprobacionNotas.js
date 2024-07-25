e60e600162c8cd35638b8dcbc0735ec1409b3c0c
germdz <gerardo.montivero@gmail.com>
2024-07-24 21:26:17 -0300

    Cambiando los datos al comprobar notas.

--- public/js/proceso_modular/cargaComprobacionNotas.js
+++ public/js/proceso_modular/cargaComprobacionNotas.js
@@ -2,21 +2,41 @@ $(document).ready(function () {
     $('.check-notes-btn').click(function (e) {
         e.preventDefault();
 
-        let url = $(this).data('url'); // Obtén la URL desde el data attribute
-        let trOneProceso = $(this).data('trone'); // Obtén la URL desde el data attribute
+        let url = $(this).data('url'); // URL desde el data attribute
+        let trOneProceso = $(this).data('trone'); // URL desde el data attribute
         let proceso = $(this).data('proceso');
         let spin = $('#spin-' + proceso);
         spin.removeClass('d-none')
         spin.addClass('d-block')
+        let data = {
+            "proceso_id": proceso,
+        };
 
-console.log(trOneProceso)
+        $.ajax({
+            url: '/proceso/cambia/simular_cierre',
+            type: "POST",
+            data: data,
+            success: function (response) {
+                console.log('20')
+
+                spin.removeClass('d-block')
+                spin.addClass('d-none')
+            },
+            error: function(xhr, status, error) {
+                // este bloque de código se ejecutará cuando ocurra un error
+                spin.removeClass('d-block')
+                spin.addClass('d-none')
+            }
+
+        });
         $.ajax({
             url: url,
             type: "GET",
             success: function (response) {
                 // Aquí actualizas el <tr> con la respuesta del servidor
-                // Recuerda que necesitas tener un identificador único para seleccionar el correcto <tr>
+                // Se necesita tener un identificador único para seleccionar el correcto <tr>
                 // Por ejemplo: $('#unique-tr-id').html(response);
+                console.log('39')
                 $('#' + trOneProceso).html(response);
                 spin.removeClass('d-block')
                 spin.addClass('d-none')
