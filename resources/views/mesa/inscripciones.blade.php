9dcc8a70e79f743a7a73e78da3757c971768dc8a
germdz <gerardo.montivero@gmail.com>
2024-09-16 23:12:51 -0300

    Agregando cohorte

--- resources/views/mesa/inscripciones.blade.php
+++ resources/views/mesa/inscripciones.blade.php
@@ -1,38 +1,44 @@
 @extends('layouts.app-prueba')
 @section('content')
-<div class="container">
+    <div class="container">
 
-    @if($mesa)
-    <a href="{{ route('mesa.mesas',['id'=>$mesa->materia->carrera->id,'instancia_id'=>$instancia->id]) }}">
-        <button class="btn btn-outline-info mb-2">
-            <i class="fas fa-angle-left"></i>
-            Volver
-        </button>
-    </a>
-    <h2 class="h1 text-info">
-        Inscripciones en {{$mesa->materia->nombre}}
-    </h2>
-    @if(time() < $mesa->cierre || (time() > strtotime($mesa->fecha) && time() < $mesa->cierre_segundo))
-            <span class="text-success font-weight-bold">
+        @if($mesa)
+            <a href="{{ route('mesa.mesas',['id'=>$mesa->materia->carrera->id,'instancia_id'=>$instancia->id]) }}">
+                <button class="btn btn-outline-info mb-2">
+                    <i class="fas fa-angle-left"></i>
+                    Volver
+                </button>
+            </a>
+            <h4 class="text-info">
+                Inscripciones en {{$mesa->materia->nombre}}
+                <small><br/>
+                    ({{ $mesa->materia->año }}° año -
+                    {{ $mesa->materia->carrera->nombre }} -
+                    {{ $mesa->materia->carrera->sede->nombre }})
+                </small>
+            </h4>
+            @if(time() < $mesa->cierre || (time() > strtotime($mesa->fecha) && time() < $mesa->cierre_segundo))
+                <span class="text-success font-weight-bold">
                 Abierta - Cierre 1er llamado: {{ date("d-m-Y", $mesa->cierre)}} | Cierre 2do llamado: {{ $mesa->fecha_segundo ? date("d-m-Y", $mesa->cierre_segundo) : '-'}}
             </span>
             @else
-            <span class="text-secondary font-weight-bold">
+                <span class="text-secondary font-weight-bold">
                 Cerrada - Cierre 1er llamado: {{ date("d-m-Y", $mesa->cierre)}} | Cierre 2do llamado: {{ $mesa->fecha_segundo ? date("d-m-Y", $mesa->cierre_segundo) : '-'}}
             </span>
             @endif
             <hr>
 
             <div class="mb-3">
-            @if(Session::has('coordinador') || Session::has('seccionAlumnos') || Session::has('admin'))
+                @if(Session::has('coordinador') || Session::has('seccionAlumnos') || Session::has('admin'))
 
-                <button class="btn btn-sm btn-primary " data-bs-toggle="modal" data-bs-target="#inscribirAlumno">
-                    Inscribir alumno
-                </button>
-                @include('mesa.modals.inscribir_alumno')
-@endif
+                    <button class="btn btn-sm btn-primary " data-bs-toggle="modal" data-bs-target="#inscribirAlumno">
+                        Inscribir alumno
+                    </button>
+                    @include('mesa.modals.inscribir_alumno')
+                @endif
                 @if(Session::has('admin'))
-                    <a class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#borrar_mesa">Borrar Mesa</a>
+                    <a class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#borrar_mesa">Borrar
+                        Mesa</a>
                     @include('mesa.modals.borrar_mesa')
                 @endif
 
@@ -44,52 +50,52 @@
             </h2>
 
             @if( count($primer_llamado) > 0)
-            @if($mesa->cierre_profesor)
-            <form action="{{route('mesa.abrir_acta',['mesa_id'=>$mesa->id])}}" method="POST" class="mt-2">
-                {{method_field('PUT')}}
-                <input type="hidden" name="llamado" value="1" id="llamado">
-                <input type="submit" value="Abrir 1er llamado" class="btn btn-sm btn-warning">
-            </form>
-            @endif
-            @include('mesa.tablas.tabla_inscripciones',['inscripciones'=>$primer_llamado,'llamado'=>1,'folios'=>$folios,'materia'=>$mesa->materia])
+                @if($mesa->cierre_profesor)
+                    <form action="{{route('mesa.abrir_acta',['mesa_id'=>$mesa->id])}}" method="POST" class="mt-2">
+                        {{method_field('PUT')}}
+                        <input type="hidden" name="llamado" value="1" id="llamado">
+                        <input type="submit" value="Abrir 1er llamado" class="btn btn-sm btn-warning">
+                    </form>
+                @endif
+                @include('mesa.tablas.tabla_inscripciones',['inscripciones'=>$primer_llamado,'llamado'=>1,'folios'=>$folios,'materia'=>$mesa->materia])
             @else
-            <p>No existen inscripciones para este llamado.</p>
+                <p>No existen inscripciones para este llamado.</p>
             @endif
 
             @if(count($primer_llamado_bajas) > 0)
-            <h2 class="text-info">Primer llamado bajas</h2>
-            @include('mesa.tablas.tabla_bajas_inscripciones',['bajas'=>$primer_llamado_bajas])
+                <h2 class="text-info">Primer llamado bajas</h2>
+                @include('mesa.tablas.tabla_bajas_inscripciones',['bajas'=>$primer_llamado_bajas])
             @endif
 
             @if($mesa->instancia->segundo_llamado)
-            <h2 class="text-info">Segundo llamado</h2>
-            @if( count($segundo_llamado) > 0)
-            @if($mesa->cierre_profesor_segundo)
-            <form action="{{route('mesa.abrir_acta',['mesa_id'=>$mesa->id])}}" method="POST" class="mt-2">
-                {{method_field('PUT')}}
-                <input type="hidden" name="llamado" value="2" id="llamado">
-                <input type="submit" value="Abrir 2do llamado" class="btn btn-sm btn-warning">
-            </form>
-            @endif
-            @include('mesa.tablas.tabla_inscripciones',['inscripciones'=>$segundo_llamado,'llamado'=>2,'folios'=>$folios_segundo,'materia'=>$mesa->materia])
-            @else
-            <p>No existen inscripciones para este llamado.</p>
-            @endif
-            @if(count($segundo_llamado_bajas) > 0)
-            <h2 class="text-info">Segundo llamado bajas</h2>
-            @include('mesa.tablas.tabla_bajas_inscripciones',['bajas'=>$segundo_llamado_bajas])
-            @endif
+                <h2 class="text-info">Segundo llamado</h2>
+                @if( count($segundo_llamado) > 0)
+                    @if($mesa->cierre_profesor_segundo)
+                        <form action="{{route('mesa.abrir_acta',['mesa_id'=>$mesa->id])}}" method="POST" class="mt-2">
+                            {{method_field('PUT')}}
+                            <input type="hidden" name="llamado" value="2" id="llamado">
+                            <input type="submit" value="Abrir 2do llamado" class="btn btn-sm btn-warning">
+                        </form>
+                    @endif
+                    @include('mesa.tablas.tabla_inscripciones',['inscripciones'=>$segundo_llamado,'llamado'=>2,'folios'=>$folios_segundo,'materia'=>$mesa->materia])
+                @else
+                    <p>No existen inscripciones para este llamado.</p>
+                @endif
+                @if(count($segundo_llamado_bajas) > 0)
+                    <h2 class="text-info">Segundo llamado bajas</h2>
+                    @include('mesa.tablas.tabla_bajas_inscripciones',['bajas'=>$segundo_llamado_bajas])
+                @endif
 
             @endif
-            @else
+        @else
             <h2 class="h1 text-info">
                 La mesa no esta configurada.
             </h2>
-            @endif
-</div>
+        @endif
+    </div>
 @endsection
 @section('scripts')
-<script src="{{ asset('js/mesas/inscripcion.js') }}"></script>
-<script src="{{ asset('js/mesas/confirmacion.js') }}"></script>
-<script src="{{ asset('js/mesas/mesas.js') }}"></script>
+    <script src="{{ asset('js/mesas/inscripcion.js') }}"></script>
+    <script src="{{ asset('js/mesas/confirmacion.js') }}"></script>
+    <script src="{{ asset('js/mesas/mesas.js') }}"></script>
 @endsection
