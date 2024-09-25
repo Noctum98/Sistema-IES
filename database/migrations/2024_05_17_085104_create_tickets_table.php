73685fe00fa7da248612c7c2e996dd32c1134dc6
Ezequiel Lucero <ezequiel.noct@gmail.com>
2024-09-25 13:18:19 -0300

    migration corregida

--- database/migrations/2024_05_17_085104_create_tickets_table.php
+++ database/migrations/2024_05_17_085104_create_tickets_table.php
@@ -16,13 +16,14 @@ class CreateTicketsTable extends Migration
         Schema::create('tickets', function (Blueprint $table) {
             $table->uuid('id');
             $table->primary('id');
-            $table->foreignId('user_id')->constrained('users');
+            $table->unsignedInteger('user_id');
             $table->foreignUuid('estado_id')->constrained('estado_tickets');
             $table->foreignUuid('categoria_id')->constrained('categorias_tickets');
             $table->string('asunto');
             $table->text('descripcion');
             $table->string('captura')->nullable();
             $table->string('url')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
             $table->softDeletes();
             $table->timestamps();
         });
