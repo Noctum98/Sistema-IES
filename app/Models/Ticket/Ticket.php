508a56831f642451e479b66d4768bcba7a14bd30
Ezequiel Lucero <ezequiel.noct@gmail.com>
2024-09-25 17:39:51 -0300

    migration corregida

--- app/Models/Ticket/Ticket.php
+++ app/Models/Ticket/Ticket.php
@@ -99,7 +99,7 @@ class Ticket extends Model
 
     public function derivaciones(): HasMany
     {
-        return $this->hasMany(DerivacionTi::class);
+        return $this->hasMany(DerivacionTicket::class);
     }
 
     public function last_derivacion(): HasOne
