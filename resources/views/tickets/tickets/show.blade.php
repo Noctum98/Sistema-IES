f4b865f29f1f528f692ac07860c05ec8ccac77e0
Ezequiel Lucero <ezequiel.noct@gmail.com>
2024-10-09 13:06:10 -0300

    Ajustes mesas y profesores planillas

--- resources/views/tickets/tickets/show.blade.php
+++ resources/views/tickets/tickets/show.blade.php
@@ -151,7 +151,7 @@
     </div>
 </div>
 @include('tickets.tickets.modals.derivar_ticket')
-@if($ticket->derivaciones->count() > 
+@if($ticket->derivaciones->count() > 0)
     @include('tickets.tickets.modals.asignar_ticket')
 @endif
 @include('tickets.tickets.modals.historial_derivaciones')
