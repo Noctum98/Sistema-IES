bfd39eca04649bb4ee53a258d8f131a10d690053
Noctum98 <62156836+Noctum98@users.noreply.github.com>
2024-10-10 18:26:35 -0300

    Update show.blade.php

--- resources/views/tickets/tickets/show.blade.php
+++ resources/views/tickets/tickets/show.blade.php
@@ -68,8 +68,7 @@
                 @if($ticket->captura && $ticket->last_estado_ticket->identificador != 'cerrado')
                 <a class="btn btn-sm btn-primary" href="{{ route('tickets.captura',$ticket->id) }}" target="_blank">Ver captura</a>
                 @else
-                                    <a href="#" class="disabled" target="_blank" rel="noopener noreferrer">{{ $ticket->url }}</a>
-
+                    <a href="#" class="disabled" target="_blank" rel="noopener noreferrer">Ver captura</a>
                 @endif
             </dd>
             <dt class="text-lg-end col-lg-2 col-xl-3">Url:</dt>
